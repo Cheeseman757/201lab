6e738ff1d2d27125074cca2baa8f872b510d14cd
Cheeseman757 <156013482+Cheeseman757@users.noreply.github.com>
2024-02-09 09:09:41 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,33 +5,44 @@ function AppState() {
 }
 
 AppState.prototype.instantiateProducts = function () {
+
   const productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];
 
   for (let i = 0; i < productNames.length; i++) {
     if (productNames[i] === 'sweep') {
-      this.allProducts.push(new Product(productNames[i], 'png'));
+      this.allProducts.push(new Product(productNames[i], 'png'))
     } else {
-      this.allProducts.push(new Product(productNames[i]));
+      this.allProducts.push(new Product(productNames[i]))
     }
   }
-};
+
+}
 
 AppState.prototype.saveToLocalStorage = function () {
-  const dataString = JSON.stringify(this.allProducts);
-  localStorage.setItem('voteData', dataString);
-};
+  const productsJson = JSON.stringify(this.allProducts);
+  localStorage.setItem('allProducts', productsJson);
+  console.log(productsJson)
+}
 
 AppState.prototype.loadItems = function () {
-  const savedData = localStorage.getItem('voteData'); // Assuming 'voteData' is the key for your saved data
-  if (savedData) {
-    // Parse the saved data and then for each product object, recreate the Product instances
-    const parsedData = JSON.parse(savedData);
-    this.allProducts = parsedData.map(product => new Product(product.name, product.source.split('.').pop()));
+const productsJson = localStorage.getItem('allproducts');
+  if (productsJson) {
+    const productsArray = JSON.parse(productsJson);
+this.allProducts = productsArray.map(product => new Product(product.name, product.source.split('.').pop()));
   } else {
-    // If there is no saved data, instantiate new products
     this.instantiateProducts();
   }
-};
+}
+    
+  // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
+  function fetchData() {
+    JSON.stringify(localStorage.getItem('allProducts'));
+  }
+
+  this.instantiateProducts();
+
+}
+
 
 function Product(name, fileExtension = 'jpg') {
   this.name = name;
